a564f2b14bdae0bd8ff01e745e0e8175d9671a98
Alex Siryi <desertkun@gmail.com>
2016-10-21 03:01:48 +0300

    Fixed host default

--- sql/hosts.sql
+++ sql/hosts.sql
@@ -3,5 +3,6 @@ CREATE TABLE `hosts` (
   `host_name` varchar(128) NOT NULL,
   `internal_location` varchar(255) NOT NULL DEFAULT '',
   `geo_location` point NOT NULL,
+  `host_default` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`host_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
